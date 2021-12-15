a5aba77bdc9a40e1de54923f4a16b5f21d94183c
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-15 22:33:19 +0600

    Login, register Authenticated with jwt token & password encryption in backend

---
+++ controllers/userController.js
@@ -0,0 +1,55 @@
+const ErrorHander = require("../backend/utils/errorHandler");
+const catchAsyncErrors = require("../backend/middleware/catchAsyncErrors");
+const User = require("../backend/models/userModel");
+const sendToken = require("../backend/utils/jwtToken");
+
+//Register User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name, email, password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profilepicUrl"
+    }
+  });
+  sendToken(user, 201, res);
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHander("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+  sendToken(user, 200, res);
+});
+
+
+// Logout User
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
