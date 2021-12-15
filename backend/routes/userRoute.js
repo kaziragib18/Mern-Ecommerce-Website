0d93ee87b3a1ad0369fbaa40348e4017c71f251d
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-16 01:06:32 +0600

    Used nodeMailer to Reset password if forgotten

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,9 +4,13 @@ const { registerUser, loginUser, logout, forgotPassword, resetPassword, } = requ
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+
 router.route("/login").post(loginUser);
+
 router.route("/logout").get(logout);
+
 router.route("/password/forgot").post(forgotPassword);
+
 router.route("/password/reset/:token").put(resetPassword);
 
 module.exports = router;
